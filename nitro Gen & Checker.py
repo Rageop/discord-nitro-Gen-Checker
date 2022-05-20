c0e80a2b2f27f4258c205dd6b781621ef8b745c8
nobLe <44553489+n2bLe@users.noreply.github.com>
2022-05-20 20:50:53 +0100

    Add files via upload

---
+++ nitro Gen & Checker.py
@@ -0,0 +1,38 @@
+from opcode import opname
+from os import link
+import random
+import re
+import string
+from turtle import st
+import requests
+
+
+num = int(input('how many codes do you want to generate ? : '))
+
+
+# get 19 unique chars , append to code
+#write them to a file
+
+file = open('NitroCodes.txt','w',encoding='utf-8')
+for i in range(num):
+    code = "".join(random.choices(
+            string.ascii_uppercase + string.digits + string.ascii_lowercase,
+            k = 19
+        ))
+    file.write(f'https://discord.gift/{code}\n')
+print(f'{num} codes were generated.')
+
+with open('NitroCodes.txt','r') as file:
+    for line in file.readlines():
+        nitro = line.strip('\n')
+        url = f"https://discordapp.com/api/v9/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
+        r = requests.get(url)
+        if r == 20:
+            print(f'This code is Valid !  {nitro}')
+        else :
+            print(f'This code is Invalid ! {nitro}')
+    
+
+
+
+
